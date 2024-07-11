539b752acaa507f6d7a72acddd622fd1d18bfc61
Perevyazko <a.perevyazko@gmail.com>
2024-07-11 13:21:50 +0300

    setting base

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ load_dotenv()
 bot = Bot(token=os.getenv('TOKEN_BOT'))
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-ADMIN_CHAT_ID = "5521511837"
+# ADMIN_CHAT_ID = "5521511837"
+ADMIN_CHAT_ID = "674501380"
 
 base = sq.connect('data.db')
 cur = base.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS files(file_id TEXT PRIMARY KEY, user_id INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS files(message_id TEXT PRIMARY KEY, user_id INTEGER, photo_id TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS admins(user_id INTEGER PRIMARY KEY, brand TEXT )")
 base.commit()
 
 
@@ -34,6 +36,38 @@ async def start(message: types.Message):
                                         url=f"https://perevyazko1.github.io/bot-acc-front")))
     await message.answer(f'Привет {message.from_user.first_name} для отправки формы нажми на кнопку 👇',
                          reply_markup=markup)
+
+
+@dp.message_handler(commands="registration_admin")
+async def start(message: types.Message):
+    restore = types.InlineKeyboardButton("restore", callback_data=f"restore")
+    xiaomi = types.InlineKeyboardButton("xiaomi", callback_data=f"xiaomi")
+    samsung = types.InlineKeyboardButton("samsung", callback_data=f"samsung")
+
+    # Создание разметки с двумя кнопками в одном ряду
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    markup.add(restore, xiaomi, samsung)
+
+    await message.answer(
+        f"Выбери Брэнд за который ты будешь отвечать. ",
+        reply_markup=markup
+    )
+
+
+@dp.callback_query_handler(lambda c: c.data in ['restore', 'xiaomi', 'samsung'])
+async def process_callback(callback_query: types.CallbackQuery):
+    # Получаем данные из callback_query
+    brand = callback_query.data
+    user_id = callback_query.from_user.id
+    print(brand, user_id)
+    cur.execute("INSERT INTO admins (user_id, brand) VALUES (?, ?)", (user_id, brand))
+    base.commit()
+
+    # Отправляем ответ пользователю
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, f"Вы зарегистрированы админом в брэнде: {brand}")
+    await bot.delete_message(chat_id=callback_query.message.chat.id,
+                             message_id=callback_query.message.message_id)
 
 
 @dp.message_handler(commands="send_form")
@@ -51,8 +85,15 @@ async def web_app(message: types.Message):
                       f"Ссылка на товар : {data_form['reference']}\n\n"
                       f"О товаре:\n\n {data_form['about']} ")
     # Пересылаем изображение админу
-    if data_form['photo'] != "":
-        await bot.send_photo(chat_id=ADMIN_CHAT_ID, photo=data_form['photo'], caption=send_form_text)
+    # await bot.send_photo(chat_id=ADMIN_CHAT_ID, photo=data_form['photo'], caption=send_form_text)
+
+    if data_form['user_id'] != "":
+        user_id = data_form['user_id']
+        photo_id = data_form['photo']
+        message = await bot.send_photo(chat_id=ADMIN_CHAT_ID, photo=data_form['photo'], caption=send_form_text)
+
+        cur.execute("INSERT INTO files (message_id, user_id, photo_id) VALUES (?, ?, ?)", (message.message_id, user_id, photo_id))
+        base.commit()
     else:
         await bot.send_message(chat_id=ADMIN_CHAT_ID, text=send_form_text)
     sent_message = await message.answer("Форма успешно отправлена в случае необходимости с тобой свяжутся")
@@ -62,15 +103,14 @@ async def web_app(message: types.Message):
 
 @dp.message_handler(content_types=types.ContentType.PHOTO)
 async def handle_photo(message: types.Message):
-    photo_id = message.photo[-1].file_unique_id  # Получаем file_id самой крупной версии изображения
+    photo_id = message.photo[-1].file_id  # Получаем file_id самой крупной версии изображения
 
     user_id = message.from_user.id
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    cur.execute("INSERT INTO files (file_id, user_id) VALUES (?, ?)", (photo_id, user_id))
-    base.commit()
     markup.add(types.KeyboardButton("открыть форму запроса",
                                     web_app=WebAppInfo(
-                                        url=f"https://perevyazko1.github.io/bot-acc-front#{photo_id}")))
+                                        url=f"https://perevyazko1.github.io/bot-acc-front#{user_id}/{photo_id}")))
     sent_message = await message.answer(f'Отлично! Теперь нажми кнопку для заполнения формы 👇',
                                         reply_markup=markup)
     await asyncio.sleep(10)
@@ -86,13 +126,16 @@ async def reply_to_user(message: types.Message):
     if message.reply_to_message:
 
         # Получаем ID пользователя из текста оригинального сообщения
-        print(message)
-        photo = message.reply_to_message.photo[3]
-        photo_id = photo['file_unique_id']
-        cur.execute("SELECT user_id FROM files WHERE file_id = ?", (photo_id,))
+        print(message.reply_to_message.message_id)
+        # photo = message.reply_to_message.photo[3]
+
+        # photo_id = photo['file_unique_id']
+        current_message = message.reply_to_message.message_id
+        cur.execute("SELECT user_id FROM files WHERE message_id = ?", (current_message,))
         result = cur.fetchone()
         user_id = result[0]
-
+        print(user_id)
+        #
         admin_message = message.text
 
         # Отправляем ответ пользователю
